d834f17b98971b2f59b88bd04454d5dbe19edcab
Liam Barry Allan <mrliamallan@live.co.uk>
2022-07-28 14:33:43 -0500

    More exports
    Signed-off-by: Liam Barry Allan <mrliamallan@live.co.uk>

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 exports.linter = require(`./src/linter`);
 exports.parser = require(`./src/parser`);
+exports.specs = require(`./src/models/specs`);
+exports.ContentRange = require(`./src/models/ContentRange`);
+exports.tags = require(`./src/models/tags`);
+exports.fixed = require(`./src/models/fixed`);
